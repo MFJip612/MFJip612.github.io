15e30fe48ac188bdfb03b04a6667b5edd74501d0
MFJip612 <sgsssvn@gmail.com>
2025-12-09 15:19:12 +0900

    feat: 更新页脚信息，添加位置图标并调整样式

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,16 +1,7 @@
 <template>
     <Spotlight />
-    <div class="personal-information">
-        <span class="material-symbols-outlined"> location_on </span>
-        Guangzhou, China
-    </div>
 </template>
 <style scoped>
-.personal-information {
-    position: fixed;
-    justify-content: center;
-    align-items: center;
-}
 </style>
 <script setup>
 import Spotlight from "@/components/spotlight.vue";
