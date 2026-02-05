2f6ffd3fef6ec05553441521c33635128c89a438
MFJip612 <sgsssvn@gmail.com>
2026-02-05 19:54:50 +0900

    ``` feat(index): 添加Google广告功能
    - 在head标签中添加Google Adsense账户meta标签
    - 添加Google Adsense脚本引用
    - 配置crossorigin属性以支持广告加载
    
    BREAKING CHANGE: 移除产品页面的元数据配置文件
    ```

---
+++ src/components/Ads.vue
@@ -0,0 +1,8 @@
+<template>
+    <ins class="adsbygoogle" :data-ad-client="adClient" :data-ad-slot="adSlot" :data-ad-format="adFormat"
+        :style="adStyle"></ins>
+</template>
+<script>
+
+</script>
+<style></style>
